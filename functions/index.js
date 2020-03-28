6e618c4bc79e8d8802df962a818fe9fcfcc3cd55
github username <github email address>
2020-03-28 00:12:06 -0400

    cred removed

--- functions/index.js
+++ functions/index.js
@@ -1,23 +1,22 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-const serviceAccount = require('/Users/Hi/Desktop/App/Social-Ape/ServiceAccountKey.json');
+// const serviceAccount = require('/Users/Hi/Desktop/App/Social-Ape/ServiceAccountKey.json');
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
+const express = require('express');
+
+const app = express();
+
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount)
+// });
 
 // const db = admin.firestore();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
+admin.initializeApp();
 
-exports.getScreams = functions.https.onRequest((req, res) => {
+app.get('/screams' , (req, res) => {
     admin.firestore().collection('screams').get()
-    .then(data => {
+    .then((data) => {
 
         let screams = [];
 
@@ -28,20 +27,21 @@ exports.getScreams = functions.https.onRequest((req, res) => {
         return res.json(screams);
     })
     .catch((err) => console.error(err));
-}) 
+});
+
 
 exports.createScream = functions.https.onRequest((req, res) => {
-
+    
     if (req.method !== 'POST'){
         return res.status(400).json({ error: 'Method not allowed'});
     };
-
+    
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
         createdAt: admin.firestore.Timestamp.fromDate(new Date())
     };
-
+    
     admin
     .firestore()
     .collection('screams')
@@ -54,3 +54,22 @@ exports.createScream = functions.https.onRequest((req, res) => {
         console.error(err);
     });
 }); 
+
+exports.api = functions.https.onRequest(app);
+
+
+// exports.getScreams = functions.https.onRequest((req, res) => {
+//     admin.firestore().collection('screams').get()
+//     .then(data => {
+
+//         let screams = [];
+
+//         data.forEach((doc) => {
+//             screams.push(doc.data());
+//         });
+
+//         return res.json(screams);
+//     })
+//     .catch((err) => console.error(err));
+
+// });
